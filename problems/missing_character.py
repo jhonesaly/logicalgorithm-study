1b5aa873c48f60c497f7484c01900ed0a1c81ce1
Jhones <jhonesaly@gmail.com>
2023-02-01 10:57:25 -0300

    update missing_character.py

--- problems/missing_character.py
+++ problems/missing_character.py
@@ -1,15 +1,25 @@
+# Desafio: https://www.geeksforgeeks.org/missing-characters-make-string-pangram/
 # Pangram is a sentence containing every letter in the English alphabet. 
 # Given a string, find all characters that are missing from the string, i.e., 
 # the characters that can make the string a Pangram. 
 # We need to print output in alphabetic order.
 
+import string
+
+
 problem_input = 'The quick brown fox jumps'
 
-caractere_list = [str for str in problem_input]
+char_list = problem_input.lower()
+char_list = [str for str in char_list]
+char_list = set(char_list)
 
-caractere_list = list(set(caractere_list))
+char_all = set(string.ascii_letters)
 
-print(caractere_list)
+# print(char_all)
+# print(char_list)
+
+char_missing = char_all.difference(char_list)
+print(char_missing)
 
 
-# problem_output = 
+# problem_output = qweqweqwe
