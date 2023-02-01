96d31a56efc83dcbe098597f392af7db3df6a853
Jhones <jhonesaly@gmail.com>
2023-02-01 15:22:16 -0300

    update bubblesort.py

--- algorithm/bubblesort.py
+++ algorithm/bubblesort.py
@@ -4,10 +4,13 @@
 # complexity is quite high.
 
 import os
+from modules.search import *
 
 os.system('cls')
 
-problem_input = [5, 1, 4, 2, 8]
+# problem_input = [8, 5, 4, 2, 1]
+
+problem_input = gen_rand_list(20, 99)
 
 # Solution 1:
 
@@ -18,18 +21,24 @@ problem_input = [5, 1, 4, 2, 8]
 
 list_raw = problem_input
 
-while ... :
-    for i in range(len(list_raw)):
-        print(i)
-        try:
-            if list_raw[i] > list_raw[i+1]:
-                num_l = list_raw[i]
-                num_r = list_raw[i+1]
-                list_raw[i] = num_r
-                list_raw[i+1] = num_l
-        except:
-            break
+
+for x in range(len(list_raw)):    
+    print(f'*****\nentrando no ciclo {x+1}')
+    for i in range(len(list_raw)-1):
+        print(f'---\nindex: {i}')
+        print(f'i: {list_raw[i]}')
+        print(f'i+1: {list_raw[i+1]}')
+        if list_raw[i] > list_raw[i+1]:
+            print('maior que o sucessor')
+            num_l = list_raw[i]
+            num_r = list_raw[i+1]
+            list_raw[i] = num_r
+            list_raw[i+1] = num_l
+        else:
+            print('não é maior que o sucessor')
+    
+    print(f'fim do ciclo {x+1}')
 
 print(list_raw)
 
-problem_answer = [1, 2, 4, 5, 8] 
+# problem_answer = [1, 2, 4, 5, 8] 
