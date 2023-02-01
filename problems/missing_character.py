8511e70d7f5549513b63bc9c7b311796402a6b93
Jhones <jhonesaly@gmail.com>
2023-02-01 11:33:43 -0300

    upd missing_characters.py

--- problems/missing_character.py
+++ problems/missing_character.py
@@ -5,7 +5,18 @@
 # We need to print output in alphabetic order.
 
 import string
+import random
 
+# char_all = set(string.ascii_letters)
+# digi_all = set(string.digits)
+# punct_all = set(string.punctuation)
+# str_all = char_all | digi_all | punct_all
+# print(str_all)
+
+#size = 10
+#caracteres = string.ascii_letters + string.digits + string.punctuation
+#string_aleatoria = "".join(random.choices(caracteres, k=size))
+# print(string_aleatoria)
 
 problem_input = 'The quick brown fox jumps'
 
@@ -13,13 +24,12 @@ char_list = problem_input.lower()
 char_list = [str for str in char_list]
 char_list = set(char_list)
 
-char_all = set(string.ascii_letters)
-
-# print(char_all)
-# print(char_list)
+char_all = set(string.ascii_lowercase)
 
 char_missing = char_all.difference(char_list)
-print(char_missing)
+char_missing = list(char_missing)
+char_missing.sort()
+char_missing = "".join(char_missing)
 
-
-# problem_output = qweqweqwe
+problem_output = char_missing
+print(problem_output)
