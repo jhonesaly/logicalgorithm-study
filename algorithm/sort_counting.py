e6161f061aec6cdb328e16845a6045c620308be1
Jhones <jhonesaly@gmail.com>
2023-02-04 08:37:15 -0300

    Update sort_counting.py

--- algorithm/sort_counting.py
+++ algorithm/sort_counting.py
@@ -4,5 +4,26 @@
 # It works by counting the number of objects having distinct key values (a kind of hashing). 
 # Then do some arithmetic operations to calculate the position of each object in the output sequence. 
 
-problem_input = {1, 4, 1, 2, 7, 5, 2}
+problem_input = [1, 4, 1, 2, 7, 5, 2]
 
+key_index = range(min(problem_input),max(problem_input))
+print(key)
+
+key_zeros = 
+
+if key_index[0] = problem_input[0]:
+
+
+problem_output = 0
+
+answer = sorted(problem_input)
+# print(answer)
+
+# Teste
+
+test1 = False
+
+if answer == problem_output:
+    test1 = True
+
+print(f'Teste 1: {test1}')
