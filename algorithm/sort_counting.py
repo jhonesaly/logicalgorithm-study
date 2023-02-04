c119fc366ec6bf18b0c77cd78b2fa905817d3366
Jhones <jhonesaly@gmail.com>
2023-02-04 08:06:05 -0300

    Add sort_counting.py

---
+++ algorithm/sort_counting.py
@@ -0,0 +1,8 @@
+# Algoritmo: https://www.geeksforgeeks.org/counting-sort/
+
+# Counting sort is a sorting technique based on keys between a specific range. 
+# It works by counting the number of objects having distinct key values (a kind of hashing). 
+# Then do some arithmetic operations to calculate the position of each object in the output sequence. 
+
+problem_input = {1, 4, 1, 2, 7, 5, 2}
+
