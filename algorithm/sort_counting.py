2eb18ba6239504373bcb05c71e5331cd59d4286b
Jhones <jhonesaly@gmail.com>
2023-02-04 09:05:59 -0300

    Update sort_counting.py

--- algorithm/sort_counting.py
+++ algorithm/sort_counting.py
@@ -6,15 +6,28 @@
 
 problem_input = [1, 4, 1, 2, 7, 5, 2]
 
-key_index = range(min(problem_input),max(problem_input))
-print(key)
+# Criando chave 
 
-key_zeros = 
+key_index = list(range(min(problem_input),max(problem_input)+1))
+key_zeros = list(0 for i in range(len(key_index)))
 
-if key_index[0] = problem_input[0]:
+for i1 in range(len(problem_input)):
+    for i2 in range(len(key_index)):
+        if key_index[i2] == problem_input[i1]:
+            key_zeros[i2] += 1
 
+problem_output = [0 for i in range(len(problem_input))]
 
-problem_output = 0
+for i1 in range (len(problem_output)):
+    
+    if key_zeros[i1] != 0:
+        problem_output[i1] = key_index[i1]
+        key_zeros[i1] -= 1
+
+# print(key_index)
+print(key_zeros)
+print(problem_output)
+
 
 answer = sorted(problem_input)
 # print(answer)
