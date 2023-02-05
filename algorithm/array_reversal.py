0743890ef2ed6a1d52163f27c884509705f615be
Jhones <jhonesaly@gmail.com>
2023-02-05 10:40:21 -0300

    add array_reversal

---
+++ algorithm/array_reversal.py
@@ -0,0 +1,37 @@
+import os
+from modules.gen_rand_list import *
+
+os.system('cls')
+
+# Criando o input: 
+
+# problem_input = gen_rand_list(5, 99)
+
+problem_input1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+problem_input2 = 3
+
+print(f'\nentradas: {problem_input}\n')
+
+###
+
+problem_output = 0
+
+
+
+print(f'\nsaída: {problem_output}\n')
+
+###
+
+answer = [8, 9, 10, 1, 2, 3, 4, 5, 6, 7]
+
+print(f'\nresposta: {answer}\n')
+
+
+# Teste
+
+test1 = False
+
+if answer == problem_output:
+    test1 = True
+
+print(f'\nTeste 1: {test1}\n')
