8787dc9a8143011f393a7fa12763770d934ff223
Jhones <jhonesaly@gmail.com>
2023-02-08 15:50:04 -0300

    update greedy_minimun_spanning.py

--- algorithm_problems/greedy_minimun_spanning.py
+++ algorithm_problems/greedy_minimun_spanning.py
@@ -1,21 +1,23 @@
 import networkx as nx
-from modules.gen_rand_graph import *
+import sys
 
+sys.path.append('modules')
+import modules.gen_rand_graph
 
-# Executa o algoritmo de Prim
-G = nx.Graph()
-G.add_nodes_from(range(len(x)))
+# # Executa o algoritmo de Prim
+# G = nx.Graph()
+# G.add_nodes_from(range(len(x)))
 
-# Adiciona as arestas com seus pesos
-for i, (orig, dest) in enumerate(todas_arestas):
-    peso = matriz_pesos[orig][dest]
-    G.add_edge(orig, dest, weight=peso)
+# # Adiciona as arestas com seus pesos
+# for i, (orig, dest) in enumerate(todas_arestas):
+#     peso = matriz_pesos[orig][dest]
+#     G.add_edge(orig, dest, weight=peso)
 
-mst = nx.minimum_spanning_tree(G, weight='weight')
+# mst = nx.minimum_spanning_tree(G, weight='weight')
 
-# Desenha as arestas da Minimum Spanning Tree com cor vermelha
-for u, vert in mst.edges:
-    plt.plot([x[u], x[vert]], [y[u], y[vert]], 'r-')
+# # Desenha as arestas da Minimum Spanning Tree com cor vermelha
+# for u, vert in mst.edges:
+#     plt.plot([x[u], x[vert]], [y[u], y[vert]], 'r-')
 
-# Mostra o gráfico
-plt.show()
+# # Mostra o gráfico
+# plt.show()
